ab3526216253dbc265e60423c00e669867112531
SamuelVitor02 <127554098+SamuelVitor02@users.noreply.github.com>
2024-08-29 01:29:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let anoAtual =2024;
+const nomeCompleto = "Samuel Vitor Oliveira de Goes"
+let idade = 16
+const cursoFaculdade = "Direito";
+let cidadeAtual = "São José dos Pinhas";
+console.log(anoAtual);
+
+console.log(`Olá, eu sou o ${nomeCompleto} Eu tenho ${idade} anos, moro em ${cidadeAtual} quero ano que vem estar cursando ${cursoFaculdade}`);
+
+anoAtual = 2025;
+
+idade = 17;
+cidadeAtua
