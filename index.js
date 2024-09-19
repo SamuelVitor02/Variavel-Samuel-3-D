968d952cb9d50e229f86e8cb41802837eb74ce9e
SamuelVitor02 <127554098+SamuelVitor02@users.noreply.github.com>
2024-09-19 01:34:36 +0000

    18/09/2024

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-let anoAtual =2024;
-const nomeCompleto = "Samuel Vitor Oliveira de Goes"
-let idade = 16
+let anoAtual = 2024;
+const nomeCompleto = "Samuel Vitor";
+let idade = 17;
 const cursoFaculdade = "Direito";
-let cidadeAtual = "São José dos Pinhas";
-console.log(anoAtual);
+let cidadeAtual = "São José dos Pinhais";
 
-console.log(`Olá, eu sou o ${nomeCompleto} Eu tenho ${idade} anos, moro em ${cidadeAtual} quero ano que vem estar cursando ${cursoFaculdade}`);
+console.log(`Olá eu sou o ${nomeCompleto}, eu tenho ${idade} anos, moro em ${cidadeAtual}, ano que vem estar cursando ${cursoFaculdade}.`);
 
 anoAtual = 2025;
 
-idade = 17;
-cidadeAtua
+let idadeFutura = 18;
+
+console.log(`Para o ano que vem, o meu nome continuará ${nomeCompleto}, vou ter ${idadeFutura} anos de idade, planejo continuar morando em ${cidadeAtual}, e planejo também estar na faculdade de ${cursoFaculdade}.`);
+
+//Agência de Viagens
+
+const nomeComprador = "Vanderley";
+const cpfComprador = 12345678;
+const idadeComprador = 21;
+const listadeDestinos = new Array(
+  "Toronto",
+  " Berlim",
+  " Las Vegas",
+  " Gramado",
+  " e Paris"
+);
+
+console.log(`Olá Senhor ${nomeComprador}, portador do CPF ${cpfComprador}, com idade de ${idadeComprador}, confirmamos que a sua passagem para ${listadeDestinos} foi confirmada.`);
+
+
+console.log(listadeDestinos[3]);
+console.log("");
+console.log("`Olá ${nomeComprador} foi adicionado mais uma viagem `")
+listadeDestinos.push("Buenos Aires");
+console.log (listadeDestinos);
+
